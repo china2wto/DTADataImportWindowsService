a23b3b882993e02e92642b4fcc1d8168ddec9682
china2wto <93147103@qq.com>
2016-12-01 11:21:37 +0800

    Add files via upload

---
+++ DTADataImportConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Configuration;
+using DTADataImport;
+using System.Threading;
+
+namespace DTADataImportConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string run_quartz = ConfigurationManager.AppSettings.Get("run_quartz");
+            if ("true".Equals(run_quartz))
+            {
+                IExample example = new XmlConfigurationExample();
+                example.Run();
+            }
+            else
+            {
+                new ProcessImport().process();
+            }
+        }
+    }
+}
